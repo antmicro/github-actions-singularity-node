1c160c64056aa531107bdc0762245364fed664c4
Adam Olech <aolech@antmicro.com>
2022-12-30 11:30:49 +0100

    additional-16-buster-slim: add openssh-client

--- additional-16-buster-slim.sh
+++ additional-16-buster-slim.sh
@@ -1,3 +1,3 @@
 apt-get -qqy update
-apt-get -qqy --no-install-recommends install git ca-certificates
+apt-get -qqy --no-install-recommends install git ca-certificates openssh-client
 rm -rf /var/lib/{apt,dpkg,cache,log}/
