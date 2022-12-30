54f2169fb337d0b37c01c91a87ef687fd941b81e
Adam Olech <aolech@antmicro.com>
2022-12-30 11:39:06 +0100

    additional-16-buster-slim: add zstd

--- additional-16-buster-slim.sh
+++ additional-16-buster-slim.sh
@@ -1,3 +1,3 @@
 apt-get -qqy update
-apt-get -qqy --no-install-recommends install git ca-certificates openssh-client
+apt-get -qqy --no-install-recommends install git ca-certificates openssh-client zstd
 rm -rf /var/lib/{apt,dpkg,cache,log}/
