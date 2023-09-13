ec710a8b8967c850a06fdfebf651169d8da1728f
Grzegorz Placzek <gplaczek@antmicro.com>
2023-09-13 14:48:06 +0200

    [#49184] Rename .env to .venv

--- make.sh
+++ make.sh
@@ -9,17 +9,17 @@ node_version=${1:-16}
 base_container=${2:-alpine3.14}
 dockerfile_dir=$top_level/docker-node/$node_version/$base_container
 sif_location=$top_level/node-$node_version-$base_container.sif
-env_activate=$top_level/.env/bin/activate
+env_activate=$top_level/.venv/bin/activate
 additional_post=$top_level/additional-$node_version-$base_container.sh
 awk_include_post='$1~/[%].+/ && d==1 { system(cat_cmd); d=0; }; $0 {print}; $1=="%post" {d=1};'
 
 if [ ! -f "$env_activate" ]; then
-    python3 -m venv .env
+    python3 -m venv .venv
     source $env_activate
     pip3 install wheel
     pip3 install -r $top_level/requirements.txt
 else
-    source $top_level/.env/bin/activate
+    source $top_level/.venv/bin/activate
 fi
 
 if [ ! -f "$additional_post" ]; then
